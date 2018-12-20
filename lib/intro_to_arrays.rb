f2710f22a7653f4742b04b74f742436e5e58e410
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-20 01:14:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,12 +1,7 @@
 =begin 
-Working with arrays
-  instantiating
-    #instantiate_new_array
-      creates a new array and returns it (FAILED - 1)
-      returns an array that is empty (FAILED - 2)
-    #array_with_two_elements
-      creates a new array with two elements in it and returns that array (FAILED - 3)
-      returns an array that has 2 objects in it (FAILED - 4) 
+
+
+
 =end
 
 def instantiate_new_array
@@ -14,8 +9,8 @@ def instantiate_new_array
   arr_one
 end
 
-
 def array_with_two_elements
   arr_two = ["one", "two"]
   arr_two
 end
+
