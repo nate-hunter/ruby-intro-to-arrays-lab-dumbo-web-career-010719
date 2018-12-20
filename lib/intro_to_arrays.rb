339ee676a6132d184de929d486b055d0cd785e16
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-20 01:20:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -34,4 +34,14 @@ def last_element (array)
 end
 
 # using ruby array methods to return values from an array
+def first_element_with_array_methods (array)
+  array.shift
+end  
+  
+def last_element_with_array_methods (array)
+  array.pop
+end
 
+def length_of_array(array)
+  array.count
+end
