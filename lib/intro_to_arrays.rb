c927a5ca63faa94ef387a8528c0271d5b5eac023
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-20 01:13:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,6 +10,12 @@ Working with arrays
 =end
 
 def instantiate_new_array
-  arr = []
-  arr
+  arr_one = []
+  arr_one
+end
+
+
+def array_with_two_elements
+  arr_two = ["one", "two"]
+  arr_two
 end
