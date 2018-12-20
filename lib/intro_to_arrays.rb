a6129907409d517ba99c246872930cbf234972ef
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-20 01:16:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,7 +1,11 @@
 =begin 
-
-
-
+ indexing
+    #first_element
+      takes in an argument of an array and returns the first element in the array using its positive index (FAILED - 5)
+    #third_element
+      takes in an argument of an array and returns the third element in the array using its positive index (FAILED - 6)
+    #last_element
+      takes in an argument of an array and returns the last element in the array using its negative index (FAILED - 7)
 =end
 
 def instantiate_new_array
@@ -12,5 +16,18 @@ end
 def array_with_two_elements
   arr_two = ["one", "two"]
   arr_two
+end
+
+# indexing
+def first_element (array)
+  array[0]
+end
+
+def third_element (array)
+  array[2]
+end
+
+def last_element (array)
+  array[-1]
 end
 
